0a9e8b94dc995120ac61121f041fbf2cc79f7aca
RainingChain <rainingchain@gmail.com>
2026-02-12 07:37:33 -0500

    .

--- src/pokemonCompletion/map/pokemonMap.ts
+++ src/pokemonCompletion/map/pokemonMap.ts
@@ -19,7 +19,7 @@ Alt + Shift + Click => reset
 
 const MAP_LINK_ICON = 'misc/teleport.png';
 
-const USE_LOCAL_IMG = window.location.href.includes('localhost') && !window.location.href.includes('prod');
+const USE_LOCAL_IMG = false; //NO_PROD window.location.href.includes('localhost') && !window.location.href.includes('prod');
 const ICON_SIZE = 24; //hardcoded in .css
 
 type LeafletEvent = {latlng:{lat:number,lng:number},originalEvent:MouseEvent};
